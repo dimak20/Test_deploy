0a51c54d8786db0d9c7c5f0375466edc2f241c3c
DSahalatyi <dsahalatyi@gmail.com>
2024-09-11 14:18:13 +0300

    Add task search on project page

---
+++ tasks/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+
+
+class TaskSearchForm(forms.Form):
+    query = forms.CharField(
+        max_length=255,
+        required=False,
+        label="",
+        widget=forms.TextInput(attrs={"placeholder": "Search.."})
+    )
--- tasks/views.py
+++ tasks/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.db.models import Case, When, Value, IntegerField, Q
 from django.views import generic
 
+from tasks.forms import TaskSearchForm
 from tasks.models import Project
 
 
@@ -12,6 +13,36 @@ class ProjectCreateView(generic.CreateView):
 class ProjectDetailView(generic.DetailView):
     model = Project
     template_name = "tasks/project_detail.html"
+
+    def get_queryset(self):
+        queryset = Project.objects.prefetch_related("tasks")
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super(ProjectDetailView, self).get_context_data(**kwargs)
+        project = self.get_object()
+        tasks = project.tasks.all()
+
+        form = TaskSearchForm(self.request.GET)
+        if form.is_valid():
+            query = form.cleaned_data["query"]
+
+            tasks = tasks.filter(
+                Q(name__icontains=query) | Q(description__icontains=query)
+            )
+
+            tasks = tasks.annotate(
+                search_priority=Case(
+                    When(name__icontains=query, then=Value(1)),
+                    When(description__icontains=query, then=Value(2)),
+                    output_field=IntegerField(),
+                )
+            ).order_by("search_priority")
+
+        context["project"] = project
+        context["tasks"] = tasks
+        context["search_form"] = TaskSearchForm()
+        return context
 
 
 class ProjectUpdateView(generic.UpdateView):
