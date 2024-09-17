8fbcc5e885d46874eb03ae0c3b9eeafef0b9e2cc
DSahalatyi <dsahalatyi@gmail.com>
2024-09-17 15:01:08 +0300

    Add invitation search & pagination, alert after successful invitation, fix list ordering

--- employees/views.py
+++ employees/views.py
@@ -63,19 +63,19 @@ class InvitationListView(ListView):
         form = InvitationSearchForm(self.request.GET)
         if form.is_valid():
             query = form.cleaned_data["query"]
-            # Prioritize email over position
-            queryset = Invitation.objects.annotate(
-                email_match=Case(
-                    When(email__icontains=query, then=1),
-                    default=0,
-                ),
-                position_match=Case(
-                    When(position__name__icontains=query, then=1),
-                    default=0,
-                )
-            ).filter(
-                Q(email__icontains=query) | Q(position__name__icontains=query)
-            ).order_by('-email_match', '-position_match')
+            if query:
+                queryset = Invitation.objects.annotate(
+                    email_match=Case(
+                        When(email__icontains=query, then=1),
+                        default=0,
+                    ),
+                    position_match=Case(
+                        When(position__name__icontains=query, then=1),
+                        default=0,
+                    )
+                ).filter(
+                    Q(email__icontains=query) | Q(position__name__icontains=query)
+                ).order_by('-email_match', '-position_match')
 
         return queryset
 
